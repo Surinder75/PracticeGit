07891f64eb032fd8a9cbde477ec47914e0361ca8
tanvirrandhawa <tanvirrandhawa@tanvirs-mbp.fios-router.home>
2021-10-02 17:53:40 -0400

    initial commit

---
+++ src/homepractice/GitClass.java
@@ -0,0 +1,18 @@
+package homepractice;
+
+public class GitClass {
+	public static void main(String[] args) {
+		
+	}
+	public static void getResult(int numOne, int numTwo) {
+
+		if (numOne > numTwo) {
+			System.out.println(numOne - numTwo);// sub
+		} else if (numOne < numTwo) {
+			System.out.println(numOne + numTwo);// add
+		} else {
+			System.out.println(numOne * numTwo);// mul
+		}
+
+}
+}
